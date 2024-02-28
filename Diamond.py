18d99cfc397c280ef518df1ba81280ee6cfd15fe
jonasgatdula <161537603+jonasgatdula@users.noreply.github.com>
2024-02-28 09:05:34 +0800

    Add files via upload

---
+++ Diamond.py
@@ -0,0 +1,15 @@
+rows = 8
+
+for i in range(1, rows):
+    for j in range(1, rows - i):
+        print(' ', end='')
+    for j in range(0, 2 * i - 1):
+        print('*', end='')
+    print()
+
+for i in range(rows - 2,0,-1):
+    for j in range(1, rows - i):
+        print(' ', end='')
+    for j in range(1, 2 * i):
+        print('*', end='')
+    print()
